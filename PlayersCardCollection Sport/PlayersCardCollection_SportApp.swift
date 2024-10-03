225bac7b7ac0d003f6b37f49ce513c6009160007
dsv <zxcv>
2024-10-03 20:30:43 +0500

    Initial Commit

---
+++ PlayersCardCollection Sport/PlayersCardCollection_SportApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayersCardCollection_SportApp.swift
+//  PlayersCardCollection Sport
+//
+//  Created by Muhammad Rizwan on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlayersCardCollection_SportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
